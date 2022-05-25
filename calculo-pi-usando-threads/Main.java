3090ed465dc48f005f06e77adacb46e2127a544a
jlss14-ifal <jlss14@aluno.ifal.edu.br>
2022-05-25 00:59:55 +0000

    Atualizando solucao

--- calculo-pi-usando-threads/Main.java
+++ calculo-pi-usando-threads/Main.java
@@ -1,4 +1,6 @@
 
+import java.lang.Math;
+
 public class Main {
 
     public static void main(String args[]) {
@@ -8,7 +10,7 @@ public class Main {
         int[] numeroThreads = new int[]{1, 2, 4, 8, 16, 32};
 
         double[] medias = new double [numeroThreads.length];
-        double[] deviosPadrao = new double [numeroThreads.length];
+        double[] desviosPadrao = new double [numeroThreads.length];
 
         //long[] tempos = new long [numeroThreads.length];
 
@@ -18,15 +20,28 @@ public class Main {
 
             System.out.println("\nCom " + numeroThreads[i] + " thread:" + "\n");
 
-            long[] execucoes = new long[5];
+            double[] execucoes = new double[5];
             
-            for(i=0;  i<5; i++){
-                execucoes[i] = new CalculoThreads().calculaComThreads(numeroThreads[i]);
+            for(int j = 0;  j<5; j++){
+                execucoes[j] =  (double) new CalculoThreads().calculaComThreads(numeroThreads[j]);
             }
-            for(j=0; j<5; j++){
-                soma += execucoes[i]
+
+            // Calcula MEDIA
+
+            double soma = 0;
+            for(int j=0; j<5; j++){
+                soma += execucoes[j];
             }
-            //medias[i]= ((i-3)**2)/5
+            medias[i]= (soma/5);
+
+            // Calcula DESVIO PADRAO
+
+            double diferenca = 0;
+            for(int j=0; j<5; j++){
+                diferenca += Math.pow((execucoes[j]-medias[i]), 2);
+            }
+
+            desviosPadrao[i] = Math.sqrt(diferenca/5);
         }
         // SAIDA
 
@@ -34,8 +49,8 @@ public class Main {
 
         for (int i = 0; i < numeroThreads.length; i++) {
 
-            System.out.println("Tempo para " + numeroThreads[i] + " Threads: " + 
-                "Media = " + medias[i] + " | " + "Desvio Padrao = " + desviosPadrao[i]);
+            System.out.println("\nTempo para " + numeroThreads[i] + " Threads: " + 
+                "\n Media = " + medias[i] + " | " + "Desvio Padrao = " + desviosPadrao[i]);
             
         }
     }
